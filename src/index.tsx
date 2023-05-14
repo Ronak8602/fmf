86183b759a85b19aa7db717c3897aee139f670cc
RonakVentura <133231138+RonakVentura@users.noreply.github.com>
2023-05-14 19:25:45 +0530

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App /> 
-  </React.StrictMode>
+  </HashRouter>
 );
-
-reportWebVitals();
